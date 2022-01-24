4f0f879ad6d3b1edff97a1dc84f484468701f633
Yasemin Apacik <98195845+yasmin82yasmin@users.noreply.github.com>
2022-01-23 21:46:32 -0500

    Update Yasmin1.java

--- src/yasmin/Yasmin1.java
+++ src/yasmin/Yasmin1.java
@@ -3,14 +3,8 @@ package yasmin;
 public class Yasmin1 {
     public static void main(String[] args) {
         System.out.println("Hello Githup");
-        System.out.println("Hello World");
-        System.out.println("Hello");
-        System.out.println("Hello world again");
-        System.out.println("gjhkjbj");
-        System.out.println("Hello java");
-        System.out.println("Hello Again");
-        System.out.println("yasemin apacik");
-        
+        System.out.println("Hello Yasemin");
+       
         
     }
 }
